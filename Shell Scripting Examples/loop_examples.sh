04934f5c96c2485f30617d5846dc4f8600565a8b
Nick Chesser <nick.chesser@outlook.com>
2017-06-23 13:34:10 +0930

    Add files via upload

--- Shell Scripting Examples/loop_examples.sh
+++ Shell Scripting Examples/loop_examples.sh
@@ -3,32 +3,22 @@
 #For Loops - Examples
 
 #Loop between a Start and End Point 
-
 for number in {1..10}
 do
         echo "$number"
 done
 
 #How to skip Numbers in a Range
-
 for number in {0..100..10}
 do
         echo "$number"
 done
 
 #Regular For Loop
-
 for ((number=0; number<100; number++)){
         echo "$number"done
 
-#Regular For Loop
-
-for ((number=0; number<100; number++)){
-        echo "$number"
-}
-
 #For Loops for files
-
 for FILE in `ls` ; do
         echo $FILE
 done
@@ -37,11 +27,8 @@ done
 
 #Simple while loop
 
-
 #Another way to do the same thing
-
 n2=0
-
 while (( $n2 < 10 ))
 do
         echo "$n2"
