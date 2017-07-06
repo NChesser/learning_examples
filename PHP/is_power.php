27336cee80ed156a4fcaa7d6d688447aaaa1d06a
Nick Chesser <nick.chesser@outlook.com>
2017-07-06 15:49:56 +0930

    Add files via upload

---
+++ PHP/is_power.php
@@ -0,0 +1,17 @@
+<?php
+function is_Power($x, $y)  
+{  
+    $final = $x; 
+    while ($x % $y == 0){  
+       $x = $x / $y;  
+    }  
+         
+    if($x == 1){  
+        print "$final is power of $y";  
+    }  
+    else{  
+        return "$final is not power of $y";  
+    }      
+}  
+is_Power(16,2);
+?>
