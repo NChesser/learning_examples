12fc9ee77d0c21a74922fdbed28cd9b5f8d9af21
Nick Chesser <nick.chesser@outlook.com>
2018-02-19 20:48:07 +1030

    Create file_deleter.sh

---
+++ Shell Scripting Examples/file_deleter.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+# script for deleting log files 
+# author: Nick Chesser
+
+# probably have to set this in the ssh
+shopt -s extglob
+
+function delete_logs() {
+  DIR=$1
+  PATTERN=$2
+  cd $DIR
+  rm !($PATTERN)
+}
+
+PATTERN=" "
+
+for last; do true; done
+
+if [ last == 'keep_recent' ];
+then
+  DATE=$(date +%Y%m)
+  PATTERN="*$DATE*.log"
+  set -- "${@:1:$(($#-1))}"
+ fi
+ 
+ DIRS="$@"
+ 
+ for DIR in $DIRS
+ do
+  delete_logs $DIR $PATTERN
+ done
